33ec943e9a2977416d511c4854db748a16b634ba
Quenton Hua <hua.quenton@gmail.com>
2023-12-27 20:27:52 -0600

    Adjustments to navbar hrefs

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -104,17 +104,28 @@
 // }
 
 // export default Navbar;
+import React, { useState, useEffect } from "react";
+import { PiMoonFill } from "react-icons/pi";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import styles from "./NavBar.module.css"; // Import CSS module
-import React, { useState, useEffect } from "react";
-import { PiMoonFill } from "react-icons/pi";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function CollapsibleExample() {
   const [scrolled, setScrolled] = useState(false);
+  const handleNavClick = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const offset = 80;
+      const sectionPosition = section.offsetTop - offset;
+      window.scrollTo({
+        top: sectionPosition,
+        behavior: "smooth",
+      });
+    }
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -132,26 +143,32 @@ function CollapsibleExample() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
-      className={`${scrolled ? styles["scrolled"] : ""} ${styles["navbar"]}
-    `}
+      className={`${scrolled ? styles["scrolled"] : ""} ${styles["navbar"]}`}
     >
       <Container className={styles["container"]}>
         <Navbar.Brand href="#home" className={styles["left-section"]}>
           QUENTON
           <br /> HUA
         </Navbar.Brand>
-{/* TODO: SWAP THE Navbar.Toggle and its content position with the Navbar.Brand below at screen size 991.5px, otherwise keep it in current */}
-        <Navbar.Toggle className={styles["middle-section"]}/>
+
+        <Navbar.Toggle className={styles["middle-section"]} />
         <Navbar.Collapse className={styles["middle-section"]}>
           <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("home")}>Home</Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("projects")}>
+              Projects
+            </Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("experience")}>
+              Experience
+            </Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("contact")}>
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
@@ -168,66 +185,3 @@ function CollapsibleExample() {
 }
 
 export default CollapsibleExample;
-
-// import React, { useState } from 'react'
-// import {
-//   CNavbar,
-//   CContainer,
-//   CNavbarBrand,
-//   CNavbarToggler,
-//   CCollapse,
-//   CNavbarNav,
-//   CNavItem,
-//   CNavLink,
-//   CDropdown,
-//   CDropdownToggle,
-//   CDropdownMenu,
-//   CDropdownItem,
-//   CForm,
-//   CFormInput,
-//   CButton,
-// } from '@coreui/react';
-// import styles from "./NavBar.module.css"; // Import CSS module
-
-// function NavBar(){
-//   const [visible, setVisible] = useState(false)
-//   return (
-//     <>
-//       <CNavbar expand="lg" colorScheme="light" className="bg-light">
-//         <CContainer fluid>
-//           <CNavbarBrand href="#">QUENTON <br/> HUA</CNavbarBrand>
-//           <CNavbarToggler
-//             aria-label="Toggle navigation"
-//             aria-expanded={visible}
-//             onClick={() => setVisible(!visible)}
-//           />
-//           <CCollapse className={`navbar-collapse ${styles["nav-items"]}`} visible={visible}>
-//             <CNavbarNav className={`me-auto mb-2 mb-lg-0`}>
-//               <CNavItem>
-//                 <CNavLink href="#" active>
-//                   Home
-//                 </CNavLink>
-//               </CNavItem>
-//               <CNavItem>
-//                 <CNavLink href="#projects">Project</CNavLink>
-//               </CNavItem>
-//               <CNavItem>
-//                 <CNavLink href="#" disabled>
-//                   Disabled
-//                 </CNavLink>
-//               </CNavItem>
-//             </CNavbarNav>
-//             <CForm className="d-flex">
-//               <CFormInput type="search" className="me-2" placeholder="Search" />
-//               <CButton type="submit" color="success" variant="outline">
-//                 Search
-//               </CButton>
-//             </CForm>
-//           </CCollapse>
-//         </CContainer>
-//       </CNavbar>
-//     </>
-//   )
-// }
-
-// export default NavBar;
